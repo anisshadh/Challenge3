4fbc1dea3631ba66c859d9891453f96a8494b67e
anisshadh <anishadh04@gmail.com>
2022-01-19 12:18:26 -0600

    added variables and pseudocode

---
+++ Develop/script.js
@@ -0,0 +1,60 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//Variables for generator
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var numericValue = '0123456789'.split('');
+var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+//need to define generatePassword function for ths project
+
+//delete cosole in the end
+function generatePassword() {
+  console.log("this is a button test"); //delete later
+  //checking password length
+  var passwordLength = window.prompt("What length should your password be? Please type a number between 8 and 128");
+  console.log(passwordLength);
+    if (passwordLength < 8 || passwordLength > 128) {
+      generatePassword();
+    }
+  //does it have uppercase value?
+  var upperCasePresent = window.confirm ("Will it contain uppercase letters?");
+
+  //does it have lowercase value?
+  var lowerCasePresent = window.confirm ("Will it contain lowercase letters?");
+
+  //does it have numeric value?
+  var numbersPresent = window.confirm ("Will it contain numbers?");
+
+  //does it have special characters?
+  var specialCharactersPresent = window.confirm ("Will it contain special characters?");
+  
+  return "the final password is returned";
+}
+
+//1. Critera prompts for passwords in sequence
+//  a. length of password
+//  b. 8 < password < 128
+//  c. ask for uppercase, lowercase, numbers/numeric, special characters separatelty
+//2.a. After each prompt input needs to be validated
+//  b. at least one prompt needs to be selected
+// 3. When prompts answered, generate password
+// 4. Assign password into password variable to be displayed
+
+// var password = (upperCasePresent, lowerCasePresent, numbersPresent, specialCharactersPresent); 
